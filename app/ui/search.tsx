5ac3ea8069f5febe0ca801303039dc9c9a77f75f
yangliwei <1280426581@qq.com>
2024-06-13 18:00:58 +0800

    搜索功能完成

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,33 @@
+/*
+ * @Author: yangliwei 1280426581@qq.com
+ * @Date: 2024-06-13 11:34:33
+ * @LastEditTime: 2024-06-13 17:51:09
+ * @LastEditors: yangliwei 1280426581@qq.com
+ * @FilePath: \nextjs-dashboard\app\ui\search.tsx
+ * Copyright (c) 2024 by yangliwei, All Rights Reserved. 
+ * @Description: 
+ */
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-
+import { usePathname } from 'next/navigation';
+import { useSearchParams,useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +36,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        defaultValue={searchParams.get('query')?.toString()}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
